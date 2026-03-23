272624216e9597889ac74a6283157e83f7da6575
Luis von Rondow <luiseduardorondow@gmail.com>
2026-03-23 00:25:07 +0000

    finishing af1_1 ex

--- lab-prog/af1_1.c
+++ lab-prog/af1_1.c
@@ -14,7 +14,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX 100
+#define MAX 1000
 #define MIN 3
 
 typedef struct
@@ -84,7 +84,7 @@ void *normalize_sentence(char *s, int sentencelen, SpecialChars *sc)
     s[j] = '\0';
 }
 
-void *rebuild_sentence(char *s, char *original, int new_sentence_len, SpecialChars *sc)
+void *rebuild_sentence(char *s, char *original, int new_sentence_len, SpecialChars *sc, int *init_p, int *end_p)
 {
     int j = 0;
 
@@ -93,7 +93,13 @@ void *rebuild_sentence(char *s, char *original, int new_sentence_len, SpecialCha
         if (i == sc->pos[j])
         {
             if (original[i] < 'A' || original[i] > 'Z')
+            {
+                if (i <= *init_p)
+                    *init_p = *init_p + 1;
+                if (i <= *end_p)
+                    *end_p = *end_p + 1;
                 memmove(s + (i + 1), s + i, new_sentence_len - i);
+            }
 
             s[i] = original[i];
             j++;
@@ -140,19 +146,69 @@ int main()
 
     int new_sentence_length = string_length(sentence);
 
-    for (int i = 1; i <= (new_sentence_length / 2); i++)
+    int init_p = -1;
+    int end_p = -1;
+    int lft = 0;
+    int rgt = new_sentence_length - 1;
+
+    while (lft < new_sentence_length - 2)
     {
-        if (sentence[i - 1] != sentence[new_sentence_length - i])
+        // posicoes espelhadas diff -> mover pointeiro da direita
+        if (sentence[lft] != sentence[rgt])
         {
-            printf("It's not a palindrome");
-            return 0;
+            rgt--;
+        }
+        else
+        {
+            int has_inc = 0;
+            // roda palindromo no range verificado
+            for (int i = 0; i <= ((rgt - lft) / 2); i++)
+            {
+                if (sentence[i + lft] != sentence[rgt - i])
+                {
+                    has_inc = 1;
+                    break;
+                }
+            }
+
+            // Verifica se o palindromo encontrada eh maior que algum previamente encontrado
+            if (has_inc == 0 && ((rgt - lft) > (end_p - init_p)))
+            {
+                init_p = lft;
+                end_p = rgt;
+            }
+
+            rgt--;
+        }
+
+        if (lft == rgt)
+        {
+            lft++;
+            rgt = new_sentence_length - 1;
         }
     }
 
-    rebuild_sentence(sentence, original, (string_length(sentence) + sc.count), &sc);
+    printf("===== %d - %d\n\n", init_p, end_p);
+    rebuild_sentence(sentence, original, (string_length(sentence) + sc.count), &sc, &init_p, &end_p);
+    printf("===== %d - %d\n\n", init_p, end_p);
 
-    printf("It's a palindrome, original sentence: \n\n");
-    printf("%s", sentence);
+    if (init_p != -1 && end_p != -1)
+    {
+        char ans[end_p - init_p + 1];
+
+        for (int i = 0; i <= end_p - init_p; i++)
+        {
+            ans[i] = sentence[i + init_p];
+        }
+
+        printf("It's a palindrome, palindrome: \n\n");
+        printf("%s", ans);
+    }
+    else
+    {
+        printf("It's not a palindrome, full sentence: \n\n");
+        printf("%s", sentence);
+    }
 
     return 0;
 }
